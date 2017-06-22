ab65948e5d439e93ce8f5fcec16f26585d983607
Artem Osta <ostart@mail.ru>
2017-06-22 14:40:26 +0300

    #20 Подготовка адаптивной графики в личном проекте. Ver.3. Сделал ретинизацию и карту

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ var nav = document.querySelector(".nav");
 var logo = document.querySelector(".main-nav__logo");
 var btn = document.querySelector(".toggler__btn");
 
+function menuToggler() {
+  nav.classList.toggle("nav--hidden");
+  logo.classList.toggle("main-nav__logo--top-state");
+  btn.classList.toggle("toggler__btn--opened");
+}
+
+window.onload = menuToggler;
 
 btn.addEventListener("click", function(event) {
     event.preventDefault();
-    nav.classList.toggle("nav--hidden");
-    logo.classList.toggle("main-nav__logo--top-state");
-    btn.classList.toggle("toggler__btn--opened");
+    menuToggler();
 });
